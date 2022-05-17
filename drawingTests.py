03e210504bd5dec7455ffb7217deccf07c39e825
CobyKuhns <105684569+CobyKuhns@users.noreply.github.com>
2022-05-17 16:41:03 -0700

    Improved efficiency

--- drawingTests.py
+++ drawingTests.py
@@ -7,6 +7,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 HOST = "192.168.5.94"
 PORT = 4000
 packetQueue = []
+drawingQueue = []
 
 def get_x_and_y(event):
     global lasx, lasy
@@ -28,11 +29,16 @@ def startConnection():
 def receiveUpdates():
     while True:
         data = (s.recv(1024).decode())
-        print(data)
-        dataList = data.split(" ")
-        print(dataList)
-        canvas.create_line((dataList[0],dataList[1],dataList[2],dataList[3]), fill = 'blue', width= 2)
-        dataList = []
+        drawingQueue.append(data)
+
+def drawingUpdates():
+    while True:
+        if drawingQueue:
+            data = drawingQueue.pop(0)
+            dataList = data.split(" ")
+            print(dataList)
+            canvas.create_line((dataList[0],dataList[1],dataList[2],dataList[3]), fill = 'blue', width= 2)
+            dataList = []
 
 def sendUpdates():
     while True:
@@ -55,4 +61,6 @@ t1 = threading.Thread(target=receiveUpdates, daemon= True)
 t1.start()
 t2 = threading.Thread(target=sendUpdates, daemon= True)
 t2.start()
+t3 = threading.Thread(target= drawingUpdates, daemon= True)
+t3.start()
 app.mainloop()
