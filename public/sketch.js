3919f4b23abc0322c46fd5f7d6eff95c3e130100
CobyKuhns <105684569+CobyKuhns@users.noreply.github.com>
2022-05-20 14:39:58 -0700

    Setting Up for Cloud

--- public/sketch.js
+++ public/sketch.js
@@ -1,13 +1,58 @@
 var socket;
-
+var penColor = {
+	r: 255,
+	g: 255,
+	b: 255,
+}
 function setup() {
-	createCanvas(400, 400);
-	background(51);
+	createCanvas(800, 800);
+	background(0);
 	
-	socket = io.connect("http://localhost:3000");
+	socket = io.connect("http://54.67.74.14:3000");
+	socket.on('mouse', newDrawing);
 }
 
-function draw() {
-	background(51);
+function newDrawing(data) {
+	console.log(data);
+	noStroke();
+	fill(255);
+	ellipse(data.x, data.y, 10, 10);
+}
+
+function mouseDragged() {
+	console.log(mouseX + ',' + mouseY)
+	var data = {
+		x: mouseX,
+		y: mouseY
+	}
+	socket.emit('mouse', data);
+	noStroke();
+	fill(255);
 	ellipse(mouseX, mouseY, 10, 10);
+}
+
+function mouseClicked() {
+	console.log(mouseX + ',' + mouseY)
+	var data = {
+		x: mouseX,
+		y: mouseY
+	}
+	socket.emit('mouse', data);
+	noStroke();
+	fill(255);
+	ellipse(mouseX, mouseY, 10, 10);
+}
+
+function touchMoved() {
+	console.log(mouseX, mouseY)
+	var data = {
+		x: mouseX,
+		y: mouseY
+	}
+	socket.emit('mouse', data);
+	noStroke();
+	fill(255);
+	ellipse(mouseX, mouseY, 10, 10);
+}
+function draw() {
 }
--- server.js
+++ server.js
@@ -17,5 +17,9 @@ var io = socket(server);
 io.sockets.on('connection', newConnection);
 
 function newConnection(socket) {
-	console.log(socket);
+	socket.on('mouse', mouseMsg);
+	function mouseMsg(data) {
+		socket.broadcast.emit('mouse', data);
+		console.log(data);
+	}
 }
