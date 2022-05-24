642b17bd6fc16d5c082bc4812ef7f44f7b30adca
CobyKuhns <105684569+CobyKuhns@users.noreply.github.com>
2022-05-23 21:25:59 -0700

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -25,8 +25,8 @@ var timerID = setInterval(function() {
 
 function loadCanvas(data) {
 	if(data !== "null") {
-		img = loadImage(data.data)
-		console.log(data.data)
+		img = loadImage(data)
+		console.log(data)
 		image(img, 0, 0);
 	}
 }
